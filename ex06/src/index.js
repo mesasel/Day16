6daf65802ee17c89e81141aadf08b08d6f628fb9
Meša Selimović <mesa_sel@hotmail.com>
2021-03-24 09:45:25 +0100

    Day16 finished

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+function firstName(firstName) {
+  return firstName.toUpperCase();
+}
+function lastName(lastName) {
+  return lastName.toLowerCase();
+}
+module.exports = {
+  firstName,
+  lastName,
+};
